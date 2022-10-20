6ac18c6f884aa14199cd3dee15ce5b7bc5719e24
Anit <anittankandasamy@gmail.com>
2022-10-20 21:01:12 +0000

    updated using parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,17 +8,21 @@ const pool = new Pool({
 });
 
 const args = process.argv.slice(2);
+const cohortName = args[0];
+const values = [`${cohortName}`];
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher,
 cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${args[0]}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`)
